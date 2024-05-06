1f3e309812c680972a4a04aa738cb42c1a9cad2a
Your Name <nnamdi.raymondoct@gmail.com>
2024-05-06 03:29:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import EmojiPicker, { Emoji } from 'emoji-picker-react';
 import './App.css';
 
-function App() {
+function App () {
+  
+  const [selectedEmoji, setSelectedEmoji] = useState('');
+
+  function onClick(emojiData) {
+      setSelectedEmoji(emojiData.unified);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className='header-content'>
+        <h2>Emoji picker React App</h2>
+        <p>Kindly select the emoji icons you want to choose</p>
+        <div className='emoji-text'>
+            {selectedEmoji ? <Emoji unified={selectedEmoji} size={52}/> : null}
+        </div>
+
+        <EmojiPicker onEmojiClick={onClick} />
     </div>
   );
-}
+};
 
 export default App;
